fc9ecf51e8fadc7171a4d54fde8359b02575af3c
koreaygj <ygj1828@gmail.com>
2022-05-01 13:16:31 +0000

    ADD:graph study baekjoon

---
+++ C++/baekjoon/silver/2606.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+int pc;
+vector<vector<int>> link(105, vector<int>(105, 0));
+vector<int> memo(105, 0);
+int answer = 1;
+void solution(int prev)
+{
+    for(int i = 1; i <= pc; i++)
+    {
+        if(memo[link[prev][i]] == 1)
+            continue;
+        if(link[prev][i] == 1)
+        {
+        answer++;
+        memo[link[prev][i]] = 1;
+        solution(i);
+        }
+    }
+    return;
+}
+int main(void){
+    int n;
+    cin >> pc;
+    cin >> n;
+    for(int i = 1;i <= n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        link[a][b] = 1;
+    }
+    solution(1);
+    cout << answer << "\n";
+}
