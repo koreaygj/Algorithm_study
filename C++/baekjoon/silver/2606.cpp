a859bd57093cabdca5041c19521ffa4a0ce4f245
koreaygj <ygj1828@gmail.com>
2022-05-03 14:26:23 +0000

    ADD:graph day2 (baekjoon)

--- C++/baekjoon/silver/2606.cpp
+++ C++/baekjoon/silver/2606.cpp
@@ -5,26 +5,29 @@ using namespace std;
 int pc;
 vector<vector<int>> link(105, vector<int>(105, 0));
 vector<int> memo(105, 0);
-int answer = 1;
+int answer = 0;
 void solution(int prev)
 {
+    cout << endl;
     for(int i = 1; i <= pc; i++)
     {
-        if(memo[link[prev][i]] == 1)
+        if(memo[i] == 1)
             continue;
-        if(link[prev][i] == 1)
+        if(link[prev][i] == 1 || link[i][prev] == 1)
         {
         answer++;
-        memo[link[prev][i]] = 1;
+        memo[i] = 1;
         solution(i);
         }
     }
     return;
 }
+
 int main(void){
     int n;
     cin >> pc;
     cin >> n;
+    memo[1] = 1;
     for(int i = 1;i <= n; i++)
     {
         int a, b;
