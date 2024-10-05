74250759e28defac591e371467f9fec989446450
koreaygj <ygj1828@gmail.com>
2024-10-05 16:20:55 +0900

    Sol : BOJ 1497 (C++)

---
+++ C++/baekjoon/silver/1497.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+  int n, c;
+  cin >> n >> c;
+  int weight[n];
+  queue<int> q;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> weight[i];
+  }
+}
