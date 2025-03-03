59d272ae931ea913332b7761579eeb0ff3d23d35
koreaygj <ygj1828@gmail.com>
2025-03-04 08:52:18 +0900

    sol: BOJ 1976(C++) 2차 풀이

--- C++/baekjoon/gold/1976.cpp
+++ C++/baekjoon/gold/1976.cpp
@@ -1,56 +1,60 @@
-#include<bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
 using namespace std;
 int Parent[201];
-int Find(int x){
-    if(x == Parent[x])
-        return x;
-    else{
-        int y = Find(Parent[x]);
-        Parent[x] = y;
-        return y;
-    }
+int Find(int x) {
+  if (x == Parent[x])
+    return x;
+  else {
+    int y = Find(Parent[x]);
+    Parent[x] = y;
+    return y;
+  }
 }
-void Union(int x, int y){
-    x = Find(x);
-    y = Find(y);
-    if(x == y)
-        return;
-    else{
-        if(x > y)
-            Parent[x] = y;
-        else
-            Parent[y] = x;
-    }
+void Union(int x, int y) {
+  x = Find(x);
+  y = Find(y);
+  if (x == y)
+    return;
+  else {
+    if (x > y)
+      Parent[x] = y;
+    else
+      Parent[y] = x;
+  }
 }
-int main(void){
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(false);
-    int n, m;
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++){
-        Parent[i] = i;
+int main(void) {
+  int n, m;
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++) {
+    Parent[i] = i;
+  }
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {
+      int a;
+      cin >> a;
+      if (a == 1) {
+        Union(i, j);
+      }
     }
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n; j++){
-            int a;
-            cin >> a;
-            if(a == 1){
-                Union(i, j);
-            }
-        }
+  }
+  bool possible = true;
+  int first;
+  cin >> first;
+  int root = Find(first);
+
+  for (int i = 1; i < m; i++) {
+    int a;
+    cin >> a;
+    if (root != Find(a)) {
+      possible = false;
+      break;
     }
-    int check;
-    cin >> check;
-    check = Find(check);
-    for(int i = 1; i < m; i++){
-        int a;
-        cin >> a;
-        if(check != Find(a)){
-            cout << "NO\n";
-            return 0;
-        }
-        check = Find(a);
-    }
+  }
+
+  if (possible) {
     cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
 }
