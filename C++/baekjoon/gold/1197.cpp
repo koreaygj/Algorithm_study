c99857828bbfbea2d670b4b0576ea5408001689f
koreaygj <ygj1828@gmail.com>
2025-03-17 14:24:23 +0900

    Sol: BOJ1197(C++)

--- C++/baekjoon/gold/1197.cpp
+++ C++/baekjoon/gold/1197.cpp
@@ -1,80 +1,73 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+
 using namespace std;
-struct trunk{
-    int st;
-    int end;
-    int cost;
+struct Trunk {
+  int st;
+  int end;
+  int cost;
+  Trunk(int st, int end, int cost) : st(st), end(end), cost(cost) {}
 };
-bool comp(trunk a, trunk b){
-    return a.cost < b.cost;
-}
-class Tree{
-    int v;
-    int e;
-    int ans, cnt = 0;
-    int find(int x);
-    bool Union(int x, int y);
-    int parent[100001];
-    vector<trunk> node;  
-    void dfs();
-    public:
-        void input();
-        void print(){
-            cout << ans << "\n";
-        }
+bool comp(Trunk a, Trunk b) { return a.cost < b.cost; }
+class Tree {
+  int v;
+  int e;
+  long long ans;
+  int find(int x);
+  bool union_parent(int x, int y);
+  int parent[100001];
+  vector<Trunk> node;
+
+ public:
+  void input();
+  void findWeight();
+  void print() { cout << ans << "\n"; }
 };
-void Tree::input(){
-    cin >> v >> e;
-    for(int i = 0; i < e; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        node.push_back({a, b, c});
-        node.push_back({b, a, c});
-    }
-    for(int i = 0; i < v + 1; i++)
-        parent[i] = i;
-    sort(node.begin(), node.end(), comp);
-    dfs();
+void Tree::input() {
+  cin >> v >> e;
+  for (int i = 0; i < e; i++) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    node.push_back(Trunk(a, b, c));
+  }
+  for (int i = 0; i < v + 1; i++) parent[i] = i;
+  sort(node.begin(), node.end(), comp);
 }
-int Tree::find(int x){
-    if(parent[x] == x)
-        return x;
-    else{
-        int y = find(parent[x]);
-        return y;
-    }
+int Tree::find(int x) {
+  if (parent[x] == x)
+    return x;
+  else {
+    return parent[x] = find(parent[x]);
+  }
 }
-bool Tree::Union(int x, int y){
-    x = find(x);
-    y = find(y);
-    if(x == y){
-        return true;
-    }
-    else{
-        if(x < y)
-            parent[y] = x;
-        else
-            parent[x] = y;
-        return false;
-    }
+bool Tree::union_parent(int x, int y) {
+  if (x == y) {
+    return true;
+  } else {
+    if (x < y)
+      parent[y] = x;
+    else
+      parent[x] = y;
+    return false;
+  }
 }
-void Tree::dfs(){
-    for(int i = 0; i < node.size(); i++){
-        trunk cur = node[i];
-        if(find(cur.st) == find(cur.end))
-            continue;
-        ans += cur.cost;
-        ++cnt;
-        Union(cur.st, cur.end);
-        if(cnt == v -1)
-            return;
+void Tree::findWeight() {
+  for (int i = 0; i < node.size(); i++) {
+    Trunk cur = node[i];
+    int x = find(cur.st);
+    int y = find(cur.end);
+    if (x != y) {
+      union_parent(x, y);
+      ans += cur.cost;
     }
+  }
 }
-int main(void){
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(false);
-    Tree mst;
-    mst.input();
-    mst.print();
+int main(void) {
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios::sync_with_stdio(false);
+  Tree mst;
+  mst.input();
+  mst.findWeight();
+  mst.print();
 }
