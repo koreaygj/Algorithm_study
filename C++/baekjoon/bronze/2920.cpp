0df37d98ac657896bd211f00044caf2dea37c585
koreaygj <ygj1828@gmail.com>
2025-03-19 15:28:34 +0900

    Sol: BOJ 2920(C++)

---
+++ C++/baekjoon/bronze/2920.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+  bool is_inc = true, is_dec = true;
+  for (int i = 0; i < 8; i++) {
+    int tmp;
+    cin >> tmp;
+    if (tmp != arr[i]) is_inc = false;
+    if (tmp != arr[7 - i]) is_dec = false;
+    if (!is_inc && !is_dec) {
+      break;
+    }
+  }
+  if (is_inc)
+    cout << "ascending\n";
+  else if (is_dec)
+    cout << "descendint\n";
+  else
+    cout << "mixed\n";
+}
