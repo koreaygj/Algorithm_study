58effef9e3ff45145a8aa44a1379cef629a362d5
koreaygj <ygj1828@gmail.com>
2025-03-19 12:18:12 +0900

    Sol: BOJ 2441(C++)

---
+++ C++/baekjoon/bronze/2441.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cin >> n;
+  for (int i = n; i > 0; i--) {
+    for (int j = 0; j < n; j++) cout << ((j < n - i) ? " " : "*");
+    cout << "\n";
+  }
+}
