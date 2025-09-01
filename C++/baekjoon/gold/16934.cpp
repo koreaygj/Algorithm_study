461f56c09cc8b768f722f3444261a98a401024b4
koreaygj <ygj1828@gmail.com>
2025-09-01 16:31:45 +0900

    resolved: BOJ 16934(C++) - 틀린 문제 다시 풀이

--- C++/baekjoon/gold/16934.cpp
+++ C++/baekjoon/gold/16934.cpp
@@ -10,26 +10,30 @@ int main() {
   map<string, pair<string, int>> alias;
   map<string, int> check_valid;
   string ans[n];
+
   for (int i = 0; i < n; i++) {
     string st;
     cin >> st;
+
     if (alias.find(st) != alias.end()) {
       string num = to_string(alias[st].second);
-      alias.insert({alias[st].first + num,
-                    make_pair(alias[st].first, alias[st].second + 1)});
-      ans[i] = alias[st].first + num;
+      alias[st] = make_pair(st + num, alias[st].second + 1);
+      ans[i] = st + num;
     } else {
       string tmp = "";
       for (int j = st.size(); j > 0; j--) {
-        if (check_valid.find(st.substr(0, j)) != check_valid.end()) {
+        if (check_valid.find(st.substr(0, j)) == check_valid.end()) {
           tmp = st.substr(0, j);
         }
         check_valid.insert({st.substr(0, j), 1});
       }
-      if (tmp.size() == 0)
-        ans[i] = st[0];
-      else
+      if (tmp.size() != 0) {
         ans[i] = tmp;
+        alias.insert({st, make_pair(tmp, 2)});
+      } else {
+        ans[i] = st;
+        alias.insert({st, make_pair(st, 2)});
+      }
     }
   }
   for (const auto key : ans) cout << key << "\n";
